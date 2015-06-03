4ac76b226fc0bb881ff91ee51aa09d1c677ebc85
Ryan Drapeau <drapeau@cs.washington.edu>
2015-06-03 00:07:34 -0700

    style

---
+++ frontend/component/CourseDetailComponent.jsx
@@ -0,0 +1,73 @@
+var React = require('react');
+var d3 = require('d3');
+var Constants = require('../Constants');
+
+var OverviewComponent = require('./OverviewComponent.jsx');
+var LinePlotComponent = require('./LinePlotComponent.jsx');
+
+/**
+ * Encapsulates the course detail screen
+ */
+var CourseDetailComponent = React.createClass({
+    getInitialState : function() {
+        return {
+            current_course_name : '',
+            current_courses : []
+        };
+    },
+
+    componentDidMount : function() {
+        this.setState({current_courses : this.getCoursesByCourseCode(this.props.course)});
+    },
+
+    componentWillReceiveProps : function(next) {
+        this.setState({current_courses : this.getCoursesByCourseCode(next.course)});
+    },
+
+    getCoursesByCourseCode : function(course) {
+        var courses = [];
+        if (course) {
+            courses = this.props.taffy(
+                        {the_course_as_a_whole : {isNumber: true}},
+                        {'course_whole_code' : {isnocase: course}}
+                    ).order('professor,datetime').limit(Constants.SEARCH_RESULT_LIMIT).get();
+        }
+
+        if (courses.length > 0) {
+            this.setState({current_course_name : courses[0].course_title});
+            this.setState({current_course_description : courses[0].course_description});
+        }
+
+        return courses;
+    },
+
+    /**
+     * Render the page
+     */
+    render: function() {
+        var headers = Constants.OVERVIEW_HEADERS.slice(0);
+        headers.splice(Constants.OVERVIEW_HEADERS.indexOf('Course Code'), 1);
+
+        var runningSum = 0.0;
+        for (var i = 0; i < this.state.current_courses.length; i++) {
+            runningSum += Math.min(this.state.current_courses[i].the_course_as_a_whole, 5);
+        }
+
+        runningSum /= this.state.current_courses.length;
+        runningSum = runningSum.toFixed(2);
+        rating = Math.floor(runningSum);
+
+        return (
+            <div className="table-container">
+                <h2><span className="courseDetailName">{this.props.course + (this.state.current_course_name ? ': ' + this.state.current_course_name : '')}</span><span className="courseDetailScore">Score: <span className={"scoreRating" + rating}>{runningSum}</span></span></h2>
+                {this.state.current_course_description != 0 &&
+                    <p className="course-description">{this.state.current_course_description}</p>
+                }
+                <LinePlotComponent divId="courseDetailPlot" detailKey="professor" current_courses={this.state.current_courses} />
+                <OverviewComponent onClickCourse={this.onClickCourse} onClickInstructor={this.props.onClickInstructor} currentData={this.state.current_courses} headers={headers} collapseKey="professor" />
+            </div>
+        );
+    }
+});
+
+module.exports = CourseDetailComponent;
